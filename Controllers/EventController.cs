e3e5a2f615bff7e7547fb903ad0b188e2f05a96b
Esra Sergiser <128863910+EsraSrg@users.noreply.github.com>
2024-12-28 04:00:47 +0300

    Create event

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -2,31 +2,51 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
-public class EventController:Controller
+public class EventController : Controller
 
 {
     private readonly IEventRepository _eventRepository;
     public EventController(IEventRepository eventRepository)
     {
-    
-        _eventRepository=eventRepository;
+
+        _eventRepository = eventRepository;
     }
-public async Task<IActionResult> Index()
-{
-    IEnumerable<Event> events=await _eventRepository.GetAll();
-    return View(events);
-}
-public async Task<IActionResult> Detail(int id){
+    public async Task<IActionResult> Index()
+    {
+        IEnumerable<Event> events = await _eventRepository.GetAll();
+        return View(events);
+    }
+    public async Task<IActionResult> Detail(int id)
+    {
 
-    Event foundEvent =await _eventRepository.GetByIdAsync(id);
-    
-    return View(foundEvent);
+        Event foundEvent = await _eventRepository.GetByIdAsync(id);
 
+        return View(foundEvent);
+
+    }
+
+    public IActionResult Create()
+    {
+
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Event newEvent)
+    {
+
+        if (!ModelState.IsValid) // modelde tanımlı kurallara uygun değilse
+        {
+            return View(newEvent); // aynı view döndürülür.
+        }
+
+        _eventRepository.Add(newEvent);
+
+        return RedirectToAction("Index");
+
+
+
+
+    }
 }
 
-public IActionResult Create()
-{
-
-    return View();
-}
-}
