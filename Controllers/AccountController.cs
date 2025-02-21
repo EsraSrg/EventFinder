04749b4b50a4d4f8916855f0003100d7e0bc9256
Esra Sergiser <128863910+EsraSrg@users.noreply.github.com>
2025-02-21 04:57:50 +0300

    Login Page

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,51 +1,47 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
-public class AccountController:Controller{
-
-
-private readonly UserManager<AppUser> _userManager;
-private readonly SignInManager<AppUser> _signInManager;
-private readonly AppDbContext _context;
-public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, AppDbContext context)
+public class AccountController : Controller
 {
-    _context=context;
-    _signInManager=signInManager;
-    _userManager=userManager;
-}
-
-
+    private readonly UserManager<AppUser> _userManager;
+    private readonly SignInManager<AppUser> _signInManager;
+    private readonly AppDbContext _context;
+    public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, AppDbContext context)
+    {
+        _context = context;
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
     public IActionResult Login()
     {
-        var response=new LoginViewModel();
+        var response = new LoginViewModel();
         return View(response);
     }
     [HttpPost]
     public async Task<IActionResult> Login(LoginViewModel loginViewModel)
-
     {
-        if(!ModelState.IsValid) return View(loginViewModel);
+        if (!ModelState.IsValid) return View(loginViewModel);
+        
+        var user=await _userManager.FindByEmailAsync(loginViewModel.EmailAddress);
 
-        var user= await _userManager.FindByEmailAsync(loginViewModel.Email);
-
-        if(user!=null){
-            // user is found, check password
-           var passwordCheck=await _userManager.CheckPasswordAsync(user,loginViewModel.Password);
-           if(passwordCheck)
-           {
-            // password correct sign in
-            var result=await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false,false);
-            if(result.Succeeded)
+        if(user!=null)
+        {// user is found, check found
+            var passwordCheck=await _userManager.CheckPasswordAsync(user,loginViewModel.Password);
+            if(passwordCheck)
             {
-                return RedirectToAction("Index","Event");
+                // password correct,sign in
+                var result=await _signInManager.PasswordSignInAsync(user,loginViewModel.Password,false,false);
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index","Event");
+                }
             }
-           }
-           // password is incorrect
-           TempData["Error"]="Wrong credentials,Please try again..";
-           return View(loginViewModel);
+            // password is incorrect
+            TempData["Error"]="Wrong credentials. Please try again";
+            return View(loginViewModel);
         }
-        // user not found
-        TempData["Error"]="Wrong credentials, please try again";
+        // User not found
+        TempData["Error"]="Wrong credentials. Please try again";
         return View(loginViewModel);
 
 
